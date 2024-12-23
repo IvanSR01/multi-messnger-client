81c0197fd38399c89e696cbec3fd589effef3e85
Ivan Nelson <ivan.sryvkin@bk.ru>
2024-12-23 09:52:08 +0300

    upd

--- src/app/dashboard/setting/page.tsx
+++ src/app/dashboard/setting/page.tsx
@@ -1,5 +1,9 @@
-import Setting from "@/screens/dashboard/setting/Setting";
+"use client";
+import dynamic from "next/dynamic";
+const Setting = dynamic(() => import("@/screens/dashboard/setting/Setting"), {
+  ssr: false,
+});
 
 export default function Page() {
-	return <Setting />
+  return <Setting />;
 }
